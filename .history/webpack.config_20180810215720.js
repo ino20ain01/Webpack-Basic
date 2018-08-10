c334b564c8e93ab69ad4792059cc0935964a445e
ino20ain01 <ino20ain01@gmail.com>
2018-08-10 22:44:24 +0700

    html plugin auto import vendor and bundle

--- .history/webpack.config_20180810215720.js
+++ .history/webpack.config_20180810215720.js
@@ -30,7 +30,7 @@ const config = {
                 exclude: '/node_modules'
             },
             {
-                use: ['style-loader', 'css-loader'],
+                use: ['css-loader, style-loader'],
                 test: /\.css$/
             },
             {
